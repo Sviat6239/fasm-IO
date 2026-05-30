a0de0a4efa4023f245320f0944c385aeca955e9b
Sviat6239 <sviatoslav6239@gmail.com>
2026-05-30 10:27:07 +0200

    first commit

---
+++ entry.asm
@@ -0,0 +1,58 @@
+format elf64 executable 3
+entry _entry
+
+segment readable executable
+    _entry:
+
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, entryprompt
+        mov rdx, entryprompt_len
+        syscall
+
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, newline
+        mov rdx, newline_len
+        syscall
+        
+        mov rax, 0
+        mov rdi, 0
+        mov rsi, userinput
+        mov rdx, 200
+        syscall
+
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, newline
+        mov rdx, newline_len
+        syscall
+
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, msg1
+        mov rdx, msg1_len
+        syscall
+
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, userinput
+        mov rdx, 200
+        syscall
+
+        mov rax, 60
+        xor rdi, rdi
+        syscall
+        
+segment readable writable
+
+    entryprompt db "Enter stm: ", 0
+    entryprompt_len = $ - entryprompt
+
+    userinput rb 200
+
+    msg1 db "You have been entered: ", 0
+    msg1_len = $ - msg1
+
+    newline db " ", 10, 0
+    newline_len = $ - newline
